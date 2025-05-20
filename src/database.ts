57a3c026bbb8e170488c97a855720788fbe28fe5
Ongamecloud <girard.mathis@outlook.com>
2025-05-19 16:38:30 -1000

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
-import { CreateTemplateRequest, UpdateTemplateRequest, EmailTemplate, ApiResponse } from '../types';
+import { CreateTemplateRequest, UpdateTemplateRequest, EmailTemplate, ApiResponse } from '../src/types';
 
 const router = Router();
 
